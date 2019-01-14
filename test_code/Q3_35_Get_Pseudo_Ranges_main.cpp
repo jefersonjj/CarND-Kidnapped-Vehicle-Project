9c9091c4453f740b815dec347d6423fc759b7691
pl80tech <pl80.tech@gmail.com>
2019-01-15 07:10:02 +0900

    Solution for quiz on lesson 3 - 35

--- test_code/Q3_35_Get_Pseudo_Ranges_main.cpp
+++ test_code/Q3_35_Get_Pseudo_Ranges_main.cpp
@@ -50,10 +50,20 @@ vector<float> pseudo_range_estimator(vector<float> landmark_positions,
             
   // loop over number of landmarks and estimate pseudo ranges
   // YOUR CODE HERE
-        
+  for (int l=0; l< landmark_positions.size(); ++l) {
+    // estimate pseudo range for each single landmark 
+    // and the current state position pose_i:
+    float range_l = landmark_positions[l] - pseudo_position;
+
+    // check if distances are positive: 
+    if (range_l > 0.0f) {
+      pseudo_ranges.push_back(range_l);
+    }
+  }
 
   // sort pseudo range vector
   // YOUR CODE HERE
-    
+  sort(pseudo_ranges.begin(), pseudo_ranges.end());
+
   return pseudo_ranges;
 }
