2aa2129ee829a342a01ad86c595559afbe5ed569
pl80tech <pl80.tech@gmail.com>
2019-01-17 07:02:47 +0900

    Solution for quiz on lesson 3 - 34

--- test_code/Q3_34_Observation_Model_Probability_main.cpp
+++ test_code/Q3_34_Observation_Model_Probability_main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "help_functions.h"
 
-float value = ?; // TODO: assign a value, the difference in distances
-float parameter = 1.0; // set as control parameter or observation measurement
+float value = 5.5; // TODO: assign a value, the difference in distances
+float parameter = 5.0; // set as control parameter or observation measurement
 float stdev = 1.0; // position or observation standard deviation
 
 int main() {
