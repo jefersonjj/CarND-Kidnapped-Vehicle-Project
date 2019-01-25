f31f90b158a4c6d8dd6e4409052a9e7ed02919c8
pl80tech <pl80.tech@gmail.com>
2019-01-25 20:56:30 +0900

    Implement predict funtion

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -77,7 +77,30 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
    *  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
    *  http://www.cplusplus.com/reference/random/default_random_engine/
    */
+  std::default_random_engine gen;
 
+  // Creates normal (Gaussian) distributions (for x, y and theta)
+  normal_distribution<double> dist_x(0, std_pos[0]);
+  normal_distribution<double> dist_y(0, std_pos[1]);
+  normal_distribution<double> dist_theta(0, std_pos[2]);
+    
+  for (int i = 0; i < num_particles; i++) {     
+    // Predict new state from given velocity and yaw_rate 
+    if (fabs(yaw_rate) > 0) {
+      particles[i].x += velocity / yaw_rate * (sin(particles[i].theta + yaw_rate * delta_t) - sin(particles[i].theta));
+      particles[i].y += velocity / yaw_rate * (cos(particles[i].theta) - cos(particles[i].theta + yaw_rate * delta_t));
+      particles[i].theta += yaw_rate * delta_t;
+    } else {
+      // In special case when yaw_rate = 0
+      particles[i].x += velocity * delta_t * cos(particles[i].theta);
+      particles[i].y += velocity * delta_t * sin(particles[i].theta);
+    }
+
+    // Add Gaussian noise
+    particles[i].x += dist_x(gen);
+    particles[i].y += dist_y(gen);
+    particles[i].theta += dist_theta(gen);
+  }
 }
 
 void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted, 
