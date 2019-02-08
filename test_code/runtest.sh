04c0198984788ab272cbcc7d3a02f236085e1dc5
pl80tech <pl80.tech@gmail.com>
2019-02-09 00:16:59 +0900

    Add description about using DEBUG compile flag

--- test_code/runtest.sh
+++ test_code/runtest.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 # Script to compile targeted source codes and run the compiled binary.
-# The command supports upto 2 source files as arguments.
+# The command supports upto 3 source files and compile flags as arguments.
 #
 # $./runtest.sh source1.cpp source2.cpp
+#
+# Add -DDEBUG like below to enable debug messages
+# $./runtest.sh source1.cpp source2.cpp -DDEBUG
 #
 # Example:
 # 
@@ -12,5 +15,8 @@
 #
 # Specifying 2 arguments:
 # $./runtest.sh Q3_37_Coding_the_Observation_Model_main.cpp multiv_gauss.cpp
+# 
+# Specifying debug option
+# $ ./runtest.sh Q3_39_Coding_the_Full_Filter_main.cpp -DDEBUG
 
-g++ $1 $2 -std=c++11 && ./a.out
+g++ $1 $2 $3 -std=c++11 && ./a.out
